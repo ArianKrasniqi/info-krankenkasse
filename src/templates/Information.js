2d2236c23fb5da784e70b080932eecbc0d3391f0
Arian Krasniqi <ariiankrasniqi@gmail.com>
2020-03-24 12:33:05 +0100

    Rendering Data in Information Template

--- src/templates/Information.js
+++ src/templates/Information.js
@@ -1,9 +1,28 @@
 import React from "react"
+import { graphql } from "gatsby"
 
 import Layout from "../components/Layout/layout"
 
-const Information = () => {
-  return <Layout>There is one page</Layout>
+export const query = graphql`
+  query($slug: String!) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      frontmatter {
+        title
+      }
+      html
+    }
+  }
+`
+
+const Information = props => {
+  return (
+    <Layout>
+      <h1>{props.data.markdownRemark.frontmatter.title}</h1>
+      <div
+        dangerouslySetInnerHTML={{ __html: props.data.markdownRemark.html }}
+      ></div>
+    </Layout>
+  )
 }
 
 export default Information
